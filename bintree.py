06ef02f30d1cb9eb1d3239739ea7d606fcb2d7b6
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2023-01-14 11:51:19 +0800

    Created add-child function.

--- bintree.py
+++ bintree.py
@@ -3,3 +3,18 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return # node already exists
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
